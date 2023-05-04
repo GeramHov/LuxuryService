23f949b0c56a8c180f0e5fa6efb5b5b4c9408107
Gueram <120452309+GeramHov@users.noreply.github.com>
2023-05-04 15:52:58 +0200

    Delete Zoo directory

--- Zoo/CONFIG/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = new PDO('mysql:host=127.0.0.1;dbname=zoo', 'root', '');
-// ALERT WHEN REQUEST FAILED
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); 
-
-return $db;
--- Zoo/treatDirt.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('CONFIG/autoload.php');
-    require_once('CONFIG/database.php');
-
-    $manager = new EnclosureManager($db);
-    $addDirt = $manager->addDirtToEnclosure($_GET['id']);
-?>
