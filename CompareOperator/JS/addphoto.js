0238abb263f9c15ace74c9a9c3be2f20e19319c3
Gueram <120452309+GeramHov@users.noreply.github.com>
2023-05-04 15:51:20 +0200

    Delete CompareOperator directory

--- CompareOperator/JS/addphoto.js
+++
@@ -1,9 +0,0 @@
-// OPEN FILE SELECTOR ON CLICK
-
-const inputSelect = document.getElementById('userphoto');
-const btn = document.getElementById('plusbtn');
-
-btn.addEventListener('click', function() {
-
-  inputSelect.click();
-});
--- CompareOperator/JS/scrolltoview.js
+++
@@ -1,9 +0,0 @@
-//  SCROLL INTO VIEW PORT SECTION PARTNERS
-
-document.addEventListener('DOMContentLoaded', function() {
-  const link = document.querySelector('#partnersbtn');
-  link.addEventListener('click', function() {
-    const target = document.querySelector('#partners');
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
